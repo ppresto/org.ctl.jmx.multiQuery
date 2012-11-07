6ff9ad1b5670c04958bb915ecfe4a37c4fe8aa48
ppresto <ppresto@centurylink.com>
2012-11-06 21:46:31 -0800

    Updating output stream to List of hashmaps.  20% completed.

--- org.ctl.jmx.multiQuery/src/main/java/jmxquery/JMXMultiQuery.java
+++ org.ctl.jmx.multiQuery/src/main/java/jmxquery/JMXMultiQuery.java
@@ -10,9 +10,13 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.*;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 
 /**
@@ -29,9 +33,8 @@ import java.util.Map;
  */
 public class JMXMultiQuery
 {
-    private final JMXProvider jmxProvider;
+    //private final JMXProvider jmxProvider;
     private final PrintStream out;
-
     private String url;
 	private int verbatim;
 	private JMXConnector connector;
@@ -57,9 +60,9 @@ public class JMXMultiQuery
 	private static final int RETURN_UNKNOWN = 3; // Invalid command line arguments were supplied to the plugin or low-level failures internal to the plugin (such as unable to fork, or open a tcp socket) that prevent it from performing the specified operation. Higher-level errors (such as name resolution errors, socket timeouts, etc) are outside of the control of plugins and should generally NOT be reported as UNKNOWN states.
 	private static final String UNKNOWN_STRING = "JMX UNKNOWN";
 
-    public JMXMultiQuery(JMXProvider jmxProvider, PrintStream out)
-    {
-        this.jmxProvider = jmxProvider;
+   //public JMXMultiQuery(JMXProvider jmxProvider, PrintStream out)
+    public JMXMultiQuery(PrintStream out) {
+        //this.jmxProvider = jmxProvider;
         this.out = out;
     }
 
@@ -67,10 +70,19 @@ public class JMXMultiQuery
     {
         try {
             parse(args);
-            connect();
+            connect(url);
             execute();
-            return report(out);
-
+            Map<String,String> jmxAttributes = new HashMap <String,String>(report(out));
+            List<Map<String,String>> jmxAttrList = new ArrayList <Map<String,String>>();
+            jmxAttrList.add(jmxAttributes);
+            for (Map<String, String> a: jmxAttrList){
+            	for (Map.Entry<String, String> v : a.entrySet()){
+            		System.out.println("\nattribute name:"+ v.getKey());
+                	System.out.println("attribute value:"+ v.getValue());
+            	}
+            }
+            //return report(out);
+            return 1;
         }
         catch(Exception ex) {
             return report(ex, out);
@@ -83,7 +95,7 @@ public class JMXMultiQuery
             catch (IOException ignore) { }
         }
     }
-    private void connect() {
+    private void connect(String url) {
     	try {
     		HashMap<String,String[]> env = new HashMap<String,String[]>();
     		String[] creds = new String[2];
@@ -118,7 +130,8 @@ public class JMXMultiQuery
 	 */
 	public static void main(String[] args)
     {
-        System.exit(new JMXMultiQuery(new DefaultJMXProvider(), System.out).runCommand(args));
+       // System.exit(new JMXMultiQuery(new DefaultJMXProvider(), System.out).runCommand(args));
+        System.exit(new JMXMultiQuery(System.out).runCommand(args));
     }
 
     private int report(Exception ex, PrintStream out)
@@ -154,17 +167,24 @@ public class JMXMultiQuery
     }
 
 
-	private int report(PrintStream out)
+	private Map<String, String> report(PrintStream out)
 	{
 		int status;
+		Map<String,String> jmxAttr = new HashMap<String,String>();
 		if(critical != null && compare( critical )){
-			status = RETURN_CRITICAL;			
+			status = RETURN_CRITICAL;	
+			jmxAttr.put("status",String.valueOf(status));
+			jmxAttr.put("statusDesc",CRITICAL_STRING);
 			out.print(CRITICAL_STRING);
 		}else if (warning != null && compare( warning)){
 			status = RETURN_WARNING;
+			jmxAttr.put("status",String.valueOf(status));
+			jmxAttr.put("statusDesc",WARNING_STRING);
 			out.print(WARNING_STRING);
 		}else{
 			status = RETURN_OK;
+			jmxAttr.put("status",String.valueOf(status));
+			jmxAttr.put("statusDesc",OK_STRING);
 			out.print(OK_STRING);
 		}
 
@@ -173,14 +193,18 @@ public class JMXMultiQuery
             out.print(' ');
             if(attributeKey !=null) {
                 out.print(attributeName +'.'+ attributeKey +"="+checkData);
+                jmxAttr.put(attributeName.toString()+'.'+attributeKey.toString(), checkData.toString());
                 if ( checkData instanceof Number) {
                     out.print (" | "+ attributeName +'.'+ attributeKey +"="+checkData);
+                    jmxAttr.put("perfData"," | "+ attributeName.toString()+'.'+attributeKey.toString()+"="+checkData.toString());
                 }
             }
             else {
                 out.print(attributeName +"="+checkData);
+                jmxAttr.put(attributeName.toString(), checkData.toString());
                 if ( checkData instanceof Number) {
                     out.print (" | "+ attributeName +"="+checkData);
+                    jmxAttr.put("perfData", " | "+ attributeName.toString() +"="+ checkData.toString());
                 }
                 shown=true;
             }
@@ -191,10 +215,13 @@ public class JMXMultiQuery
 				report((CompositeDataSupport)infoData, out);
 			else
 				out.print(infoData.toString());
+				jmxAttr.put("infoData", infoData.toString());
 		}
 		
-		out.println();
-		return status;
+		//jmxAttrList.add((Map.Entry<String, String>) jmxAttr.entrySet());
+		//out.println();
+		//return status;
+		return jmxAttr;
 	}
 
 	@SuppressWarnings("unchecked")
