b069229c07a3fb1628ca6acd745251dd97c50611
ppresto <ppresto@centurylink.com>
2012-12-07 15:47:34 -0800

    check_jmx script JAVA_HOME update.

--- org.ctl.jmx.multiQuery/scripts/check_jmxJboss.sh
+++ org.ctl.jmx.multiQuery/scripts/check_jmxJboss.sh
@@ -3,5 +3,15 @@
 # Nagios plugin to monitor Java JMX (http://java.sun.com/jmx)attributes.
 #
 RDIR=`dirname $0`
+
+if [[ ! -z $JAVA_HOME ]]; then
+  JAVA=$JAVA_HOME/bin/java
+elif [[ $(echo $(which java | grep -v "no")) ]]; then
+  JAVA=$(which java)
+else
+  echo "Can't find java in your PATH"
+  exit 1
+fi
+
 export JAVA_OPTS="-Djava.util.logging.manager=java.util.logging.LogManager -Djava.util.logging.config.file=logging.properties"
-/usr/bin/java $JAVA_OPTS -jar $RDIR/jbossMultiQuery.jar "$@"
+$JAVA $JAVA_OPTS -jar $RDIR/jbossMultiQuery.jar "$@"
